610ba01466d154c6352c176f388dc3e02358213d
Uchenna Alua <aluauchenna@yahoo.com>
2024-02-05 12:21:50 +0100

    setting up react app and first practice

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+import reactLogo from './images/logo192.png'
+
+export default function Header() {
+  return (
+    <header>
+        <nav className="nav">
+          <img src={reactLogo} className="nav-logo"/>
+          <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+  )
+}
